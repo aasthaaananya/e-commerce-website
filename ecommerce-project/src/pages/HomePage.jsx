19716c1ce62821c91866ec929281aa229555b252
Aastha <aasthaananyagandhi.risg2rohini@gmail.com>
2026-06-02 13:58:19 +0530

    fetch data from the backend

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -4,6 +4,14 @@ import "./HomePage.css";
 
 
 export function HomePage() {
+    fetch('http://localhost:3000/api/products')
+        .then((response)=>{
+            response.json().then((data)=>{
+                console.log(data);
+
+            })
+
+        })
     return (
         <>
             <Header></Header>
