28fa328284d62c373d826daa7173a133e7100dcd
Aastha <aasthaananyagandhi.risg2rohini@gmail.com>
2026-06-07 21:44:45 +0530

    covert home and checkout to async await

--- ecommerce-project/src/pages/home/HomePage.jsx
+++ ecommerce-project/src/pages/home/HomePage.jsx
@@ -1,31 +1,33 @@
 import axios from 'axios'
-import { useEffect,useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Header } from '../../components/Header'
-import {ProductsGrid} from './ProductsGrid'
+import { ProductsGrid } from './ProductsGrid'
 import "./HomePage.css";
 
 
-export function HomePage({cart}) {
-    const[products,setProducts] = useState([]);
-    
+export function HomePage({ cart }) {
+    const [products, setProducts] = useState([]);
 
-    useEffect(()=>{
-        axios.get('/api/products')
-    .then((response)=>{
-        setProducts(response.data);
 
+    useEffect(() => {
+        const getHomeData = async () => {
+            const response = await axios.get('/api/products');
+            setProducts(response.data);
+        }
+
+        getHomeData();
     });
-  
 
-    },[])
-    
-        
+
+
+
+
     return (
         <>
             <Header cart={cart} />
 
             <div className="home-page">
-              <ProductsGrid products={products} />
+                <ProductsGrid products={products} />
             </div>
 
         </>
